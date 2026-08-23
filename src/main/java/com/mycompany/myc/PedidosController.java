755be30d614f53f633c153cd666252286faaee05
LeoVltt <leovalinottip@gmail.com>
2026-08-23 16:03:13 -0300

    Vista de cargar_pedidos editado

--- src/main/java/com/mycompany/myc/PedidosController.java
+++ src/main/java/com/mycompany/myc/PedidosController.java
@@ -6,8 +6,18 @@ package com.mycompany.myc;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 /**
  * FXML Controller class
  *
@@ -15,6 +25,55 @@ import javafx.fxml.Initializable;
  */
 public class PedidosController implements Initializable {
 
+
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private TextField txtBuscarCliente;
+    @FXML
+    private TableView<?> tablaClientes;
+    @FXML
+    private TableColumn<?, ?> columClienteID;
+    @FXML
+    private TableColumn<?, ?> columClienteNombre;
+    @FXML
+    private TextField txtNombreCliente;
+    @FXML
+    private Button btnAddCliente;
+    @FXML
+    private TextField txtNombreProducto;
+    @FXML
+    private Button btnAddProducto;
+    @FXML
+    private DatePicker dpFecha;
+    @FXML
+    private TextField txtCantidad;
+    @FXML
+    private Button btnAgregar;
+    @FXML
+    private TableView<?> tablaDetalle;
+    @FXML
+    private TableColumn<?, ?> columIDProducto;
+    @FXML
+    private TableColumn<?, ?> columNombreProducto;
+    @FXML
+    private TableColumn<?, ?> columCantidadDet;
+    @FXML
+    private TableColumn<?, ?> columPrecioDet;
+    @FXML
+    private TableColumn<?, ?> columSubtotalDet;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private Button btnEditar;
+    @FXML
+    private Button btnEliminar;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private Button btnImprimir;
+    @FXML
+    private TextField txtTotal;
     /**
      * Initializes the controller class.
      */
@@ -23,4 +82,44 @@ public class PedidosController implements Initializable {
         // TODO
     }    
     
+    @FXML
+    private void buscarCliente(KeyEvent event) {
+    }
+
+    @FXML
+    private void mostrarCliente(MouseEvent event) {
+    }
+
+    @FXML
+    private void addCliente(ActionEvent event) {
+    }
+
+    @FXML
+    private void abrirProducto(ActionEvent event) {
+    }
+
+    @FXML
+    private void agregar(ActionEvent event) {
+    }
+
+    @FXML
+    private void guardar(ActionEvent event) {
+    }
+
+    @FXML
+    private void editar(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void imprimir(ActionEvent event) {
+    }
+
 }
